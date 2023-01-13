c0b4bcd57472984c9d15f4f7dcace1466401a29c
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2023-01-13 16:02:34 +0000

    Add setup.py

---
+++ pyflamegraph/__init__.py
@@ -0,0 +1,4 @@
+from .pyflamegraph import FileFormat
+from .pyflamegraph import Flamegraph
+from .pyflamegraph import StackTree
+from .pyflamegraph import TraceElement
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='pyflamegraph',
+      version='1.0',
+      description='Flame Graph library for Python',
+      author='Rosie Baish',
+      url='https://github.com/RosieBaish/pyflamegraph',
+      packages=find_packages(include=['pyflamegraph'])
+     )
